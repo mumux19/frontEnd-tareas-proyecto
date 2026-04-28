a7ac9a9bd9d2d19bf1c3882b2a2173f44f7edf84
Agustin <mumugomez19@gmail.com>
2026-04-28 16:15:14 -0300

    crear tarea formulario

--- frontEnd-tareas-proyectos/src/app/app.routes.ts
+++ frontEnd-tareas-proyectos/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CrearProyectoComponent } from './projects/crear-proyecto/crear-proyecto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'projects/crear', component: CrearProyectoComponent },
+  { path: '', redirectTo: 'projects/crear', pathMatch: 'full' }, // Redirigir a crear proyecto por defecto
+  { path: '**', redirectTo: 'projects/crear' } // Manejo de rutas no encontradas
+];
---
+++ frontEnd-tareas-proyectos/src/app/services/project.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Project, ProjectCreateRequest } from '../models/project.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = 'http://localhost:8080/projects';
+
+  createProject(project: ProjectCreateRequest): Observable<Project> {
+    return this.http.post<Project>(this.apiUrl, project).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    return throwError(() => error);
+  }
+}
